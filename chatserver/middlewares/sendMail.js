6b40c1469d11d5de80fcfbbc3cb3b95be83e3312
AdiJo2004 <143385334+AdiJo2004@users.noreply.github.com>
2025-09-03 00:37:46 +0530

    Update sendMail.js

--- chatserver/middlewares/sendMail.js
+++ chatserver/middlewares/sendMail.js
@@ -2,7 +2,7 @@ import { createTransport } from "nodemailer";
 
 const sendMail = async (email, subject, otp) => {
   const transport = createTransport({
-    host: "gmail",
+    service: "gmail",
     port: 465,
     auth: {
       user: process.env.Gmail,
